5d084887e6a2755421ab871cddd35964e3c86856
Hsmahbub <hsmahbub@gmail.com>
2023-05-20 10:49:19 +0600

    aos package is used for galary image

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import { Outlet } from 'react-router-dom'
 import Footer from './Components/Shared/Footer'
 import { ToastContainer } from 'react-toastify'
 import Navbar from './Components/Shared/Navbar'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react'
 function App() {
+  useEffect(() => {
+    AOS.init(
+      {
+        duration: 800,
+        easing: 'ease-in-out',
+        once: true
+      }
+    )
+  }, [])
   return (
     <div className='max-w-[1240px]  m-auto'>
       <ToastContainer />
--- src/Components/Galary.jsx
+++ src/Components/Galary.jsx
@@ -5,7 +5,7 @@ const GallerySection = ({ galary:images }) => {
 
     return (
         <>
-            <section className="py-10 md:px-10 px-5">
+            <section className="py-10 md:px-10 px-5" data-aos="fade-up">
                 <div className="container mx-auto">
                     <h2 className="text-3xl font-bold mb-8 text-center">Gallery</h2>
                     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
